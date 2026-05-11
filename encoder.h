77765f52470fafb526671cf1dee441be0a60f131
ThaTeo <mttbettani@gmail.com>
2026-05-11 16:09:33 +0200

    new encoder.h

--- encoder.h
+++ encoder.h
@@ -14,13 +14,16 @@ typedef enum {
 typedef struct {
     gpio_t pin_a;
     gpio_t pin_b;
-    gpio_t last_edge;
+
+    uint16_t last_a;
+    uint16_t last_b;
+
     uint16_t level_a;
     uint16_t level_b;
+
     uint8_t direction;
     int64_t ticks;
 } encoder_t;
 
-void encoder_gpio_init(const encoder_t*);
-void encoder_gpio_register_isr(const encoder_t*, channel_t, int, void(*), void(*));
-void encoder_gpio_cancel_isr(const encoder_t*, channel_t, int);
+void encoder_gpio_register_isr(const encoder_t*, gpioAlertFuncEx_t);
+void encoder_gpio_cancel_isr(const encoder_t*);
